a7fc254a1e356b9b22bd0046be8e1ba6e0a6c247
meng lei <1955854667@qq.com>
2024-10-28 23:45:45 +0800

    docs: router

--- apps/web-ele/src/router/index.ts
+++ apps/web-ele/src/router/index.ts
@@ -16,8 +16,10 @@ const router = createRouter({
   history:
     // 根据环境变量选择使用hash模式或history模式
     import.meta.env.VITE_ROUTER_HISTORY === 'hash'
-      ? createWebHashHistory(import.meta.env.VITE_BASE)
-      : createWebHistory(import.meta.env.VITE_BASE),
+      ? // 它使用URL的hash部分（即#后面的部分）来模拟完整的URL路径,而不会导致浏览器向服务器发送请求，一般格式是：https://example.com/#/about
+        createWebHashHistory(import.meta.env.VITE_BASE)
+      : // 基于HTML5的history.pushState API来实现的，无需#，请求后段url
+        createWebHistory(import.meta.env.VITE_BASE),
   // 应该添加到路由的初始路由列表。
   routes,
   // 定义了页面切换时的滚动行为
